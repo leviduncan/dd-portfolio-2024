c2efd8163a10114284ad11367f1a98308db6bcdf
darrin@darrinduncan.com <darrin@darrinduncan.com>
2024-05-14 06:25:46 -0400

    Update Layout

---
+++ src/components/aside/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav className="aside col-12 col-sm-3">
+        <div className="pad-container">Nav</div>
+    </nav>
+  )
+}
+
+export default Navigation
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main className="col-12 col-sm-9">
+    <div className="pad-container">Main</div>
+    </main>
+  )
+}
+
+export default Main
